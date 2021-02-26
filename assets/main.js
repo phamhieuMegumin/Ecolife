f4c4f32624cdd938a5040fd23ba5494dbd9906a3
Your Name <you@example.com>
2021-02-26 21:23:56 +0700

    responsive

--- assets/main.js
+++ assets/main.js
@@ -18,6 +18,7 @@ document.querySelector(".header-main").addEventListener("click", () => {
     item.classList.remove("active");
   });
 });
+
 new Swiper(".swiper-container", {
   spaceBetween: 30,
   effect: "fade",
@@ -35,51 +36,129 @@ window.addEventListener("scroll", () => {
   else navBar.classList.remove("active-drop");
 });
 new Swiper(".swiper-container--2", {
-  slidesPerView: 5,
+  slidesPerView: 1,
   spaceBetween: 30,
   navigation: {
     nextEl: ".btn__next-type-2",
     prevEl: ".btn__prev-type-2",
   },
+  breakpoints: {
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    1240: {
+      slidesPerView: 5,
+      spaceBetween: 30,
+    },
+    740: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+  },
 });
 new Swiper(".swiper-container--3", {
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 30,
   navigation: {
     nextEl: ".btn__next-type-3",
     prevEl: ".btn__prev-type-3",
   },
+  breakpoints: {
+    740: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+  },
 });
 new Swiper(".swiper-container--4", {
-  slidesPerView: 1,
+  slidesPerView: 2,
   spaceBetween: 30,
   navigation: {
     nextEl: ".btn__next-type-4",
     prevEl: ".btn__prev-type-4",
   },
+  breakpoints: {
+    740: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+  },
 });
+
 new Swiper(".swiper-container--5", {
-  slidesPerView: 4,
+  slidesPerView: 2,
   spaceBetween: 30,
   navigation: {
     nextEl: ".btn__next-type-5",
     prevEl: ".btn__prev-type-5",
   },
+  breakpoints: {
+    1240: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    600: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
 });
+
 new Swiper(".swiper-container--6", {
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 30,
   navigation: {
     nextEl: ".btn__next-type-6",
     prevEl: ".btn__prev-type-6",
   },
+  breakpoints: {
+    1240: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    740: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+  },
 });
 new Swiper(".swiper-container--7", {
-  slidesPerView: 6,
+  slidesPerView: 2,
   spaceBetween: 30,
   navigation: {
     nextEl: ".btn__next-type-7",
     prevEl: ".btn__prev-type-7",
+  },
+  breakpoints: {
+    1240: {
+      slidesPerView: 6,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 30,
+    },
+    740: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    600: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
   },
 });
 setTimeout(() => {
